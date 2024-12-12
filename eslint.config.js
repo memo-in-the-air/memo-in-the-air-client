6ab7ca85d088702710544b160e07427110fd12c8
HANSOL <hansol302@gmail.com>
2024-12-12 23:11:58 +0900

    Chore: prettier 설정 수정

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,18 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import prettier from "eslint-plugin-prettier";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {
     files: ["**/*.{js,mjs,cjs,ts}"],
+    plugins: {
+      prettier: prettier,
+    },
     rules: {
       "prefer-const": "warn",
+      "prettier/prettier": "error",
       "no-constant-binary-expression": "error",
     },
   },
