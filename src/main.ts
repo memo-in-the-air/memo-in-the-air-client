39f8ab79ddc75963457881e68d2167a7278b1774
HANSOL <hansol302@gmail.com>
2024-12-16 17:26:45 +0900

    Fix: firebase 라이브러리 삭제

--- src/main.ts
+++
@@ -1,7 +0,0 @@
-import { database } from "../firebaseConfig";
-import { get, ref } from "firebase/database";
-
-export async function getMemoList(userId: string) {
-  const snapshot = await get(ref(database, `memo/${userId}`));
-  return snapshot.val();
-}
